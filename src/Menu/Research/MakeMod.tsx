8aacd01b8b9dd2ad47cc352af6d3fc62f567d704
夷乐鑫 <1330958515@qq.com>
2023-09-11 18:02:11 +0800

    -

--- src/Menu/Research/MakeMod.tsx
+++ src/Menu/Research/MakeMod.tsx
@@ -1,33 +1,40 @@
-import { Alert, Space, Typography } from "antd";
+import { Alert, Button, Card, Divider, List, Space, Typography } from "antd";
+import { ButtonLink } from "../../Data/Components";
 
 
 const MenuItem = {
     label: "Mod制作",
     key: "/Research/MakeMod",
     page: () => <Typography.Paragraph>
-        <Alert type="warning" showIcon message="警告" description={
-            <ul>
-                <li>强烈建议配合后续推出的视频教程阅读文档，因为文档大多起笔记大纲的作用，可能会有意写的复杂化或简化。</li>
-                <li>这个是教程不是WIKI，只会讲述教程中用到部分功能，更完整深入的资料请查询WIKI。</li>
-            </ul>
-        }></Alert>
-        <Typography.Title level={2}>1. 序章</Typography.Title>
-        <blockquote>前置要求，教程简介，必读！！！</blockquote>
-        <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17621153.html" target="_blank">【COM3D2Mod教程】序章：教程简介</Typography.Link>
-        <Typography.Title level={2}>2. 入门</Typography.Title>
-        <blockquote>搞定动漫角色最重要的特征点，让别人看了能直呼老婆！</blockquote>
         <Space direction="vertical">
-            <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17647617.html" target="_blank">【COM3D2Mod教程】第一章：Mod底层原理</Typography.Link>
-            <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17660468.html" target="_blank">【COM3D2Mod教程】第二章：Mod制作概要</Typography.Link>
-            <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17689349.html" target="_blank">【COM3D2Mod教程】第三章：实战！制作身体部分（上）</Typography.Link>
-            <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17689415.html" target="_blank">【COM3D2Mod教程】第四章：实战！制作身体部分（中）</Typography.Link>
-            <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17690369.html" target="_blank">【COM3D2Mod教程】第五章：实战！制作身体部分（下）</Typography.Link>
-            视频制作中......
-        </Space>
-        <Typography.Title level={2}>3. 进阶</Typography.Title>
-        <blockquote>让老婆能穿上自己的原设衣装，让别人看了能直呼大佬！</blockquote>
-        制作中......（预计2023年底发布）
-    </Typography.Paragraph>
 
+            <Alert type="info" showIcon message="这个是教程不是WIKI，只会讲述教程中用到部分功能，更完整深入的资料请查询WIKI。" ></Alert>
+            <Card title="入门">
+                <blockquote>搞定动漫角色最重要的特征点，让别人看了能直呼老婆！</blockquote>
+                <Card title="文字教程" type="inner">
+                    <Space direction="vertical">
+                        <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17621153.html" target="_blank">【COM3D2Mod制作教程】序章：教程简介（含前置要求，必读！！！）</Typography.Link>
+                        <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17647617.html" target="_blank">【COM3D2Mod制作教程】第一章：Mod底层原理</Typography.Link>
+                        <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17660468.html" target="_blank">【COM3D2Mod制作教程】第二章：Mod制作概要</Typography.Link>
+                        <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17689349.html" target="_blank">【COM3D2Mod制作教程】第三章：实战！制作身体部分（上）</Typography.Link>
+                        <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17689415.html" target="_blank">【COM3D2Mod制作教程】第四章：实战！制作身体部分（中）</Typography.Link>
+                        <Typography.Link href="https://www.cnblogs.com/BDFFZI/p/17690369.html" target="_blank">【COM3D2Mod制作教程】第五章：实战！制作身体部分（下）</Typography.Link>
+                    </Space>
+                </Card>
+                <Card title="配套视频" type="inner">
+                    <Space direction="vertical">
+                        <ButtonLink href="https://www.bilibili.com/video/BV148411i7uy/">解读视频（序章~第三章内容）</ButtonLink>
+                        <ButtonLink href="https://www.bilibili.com/video/BV148411i7uy/">实操视频（第四章内容）</ButtonLink>
+                        <ButtonLink href="https://www.bilibili.com/video/BV148411i7uy/">实操视频（第五章内容）</ButtonLink>
+                    </Space>
+                </Card>
+            </Card>
+            <Divider dashed></Divider>
+            <Card title="进阶">
+                <blockquote>让老婆能穿上自己的原设衣装，让别人看了能直呼大佬！</blockquote>
+                制作中......（预计2023年底发布）
+            </Card>
+        </Space >
+    </Typography.Paragraph>
 }
 export default MenuItem;    
